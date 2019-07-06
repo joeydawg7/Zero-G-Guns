eeb2d548500f4c73fff01aa9de92df7b13444e11
Chris Lyons <joeydawg7@gmail.com>
2019-07-05 21:40:50 -0400

    base ass game
    no asses involved

---
+++ NoGravityGuns/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition) * -1), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(gameObject.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    //TODO: track mouse location in relation to center point of char
+    //TODO: add negative force on click
+    //TODO: raycast direction on click, check for hittables
+    //TODO: deal dmg knockback, etc,
+}
