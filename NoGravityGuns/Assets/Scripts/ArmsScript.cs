348980cebe89f3fb7c3fe4c7e1cd58cd36abdeb8
Sam Rasmussen <samrandomonium@gmail.com>
2019-07-08 00:25:03 -0400

    Small tweaks

--- NoGravityGuns/Assets/Scripts/ArmsScript.cs
+++ NoGravityGuns/Assets/Scripts/ArmsScript.cs
@@ -65,51 +65,51 @@ public class ArmsScript : MonoBehaviour
 
     }
 
-    public void Start()
-    {
+    // public void Start()
+    // {
 
-        startingEulers = transform.eulerAngles;
-
-
-    }
+    //     startingEulers = transform.eulerAngles;
 
 
-
-    IEnumerator PickupWeaponDelay()
-    {
-        canRotateGun = false;
-        SetOnEquip();
-        yield return new WaitForSeconds(0.1f);
-
-        canRotateGun = true;
-        Debug.Log(canRotateGun);
-    }
+    // }
 
 
-    //public void OnEnable()
-    //{
+
+    // IEnumerator PickupWeaponDelay()
+    // {
+    //     canRotateGun = false;
+    //     SetOnEquip();
+    //     yield return new WaitForSeconds(0.1f);
+
+    //     canRotateGun = true;
+    //     Debug.Log(canRotateGun);
+    // }
 
 
-    //    /*transform.rotation = startingTransform.rotation;
-    //    transform.position = startingTransform.position;
-    //    transform.localScale = startingTransform.localScale;*/
-    //}
+    // //public void OnEnable()
+    // //{
 
 
-    public void SetOnEquip()
-    {
-        if (GameManager.Instance.isGameStarted)
-        {
+    // //    /*transform.rotation = startingTransform.rotation;
+    // //    transform.position = startingTransform.position;
+    // //    transform.localScale = startingTransform.localScale;*/
+    // //}
 
-            facing = startingRot;
-            //shootDir = new Vector2(0, 0);
-            //aim = shootDir;
-            rotation = Quaternion.LookRotation(Vector3.forward, -shootDir);
-            rotation *= facing;
-            transform.rotation = rotation;
-        }
 
-    }
+    // public void SetOnEquip()
+    // {
+    //     if (GameManager.Instance.isGameStarted)
+    //     {
+
+    //         facing = startingRot;
+    //         //shootDir = new Vector2(0, 0);
+    //         //aim = shootDir;
+    //         rotation = Quaternion.LookRotation(Vector3.forward, -shootDir);
+    //         rotation *= facing;
+    //         transform.rotation = rotation;
+    //     }
+
+    // }
 
 
     public void SetChildrenWithAxis(int playerID)
