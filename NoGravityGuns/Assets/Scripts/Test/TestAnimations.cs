f2c9318dc361d1c156a1009204d1cf3078f0fbc5
Chris Lyons <joeydawg7@gmail.com>
2019-09-09 21:19:46 -0400

    animation testing script addded

---
+++ NoGravityGuns/Assets/Scripts/Test/TestAnimations.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimations : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    bool thing=false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!thing)
+            {
+                anim.SetBool("isHPCritical", true);
+                thing = true;
+            }
+            else
+            {
+                anim.SetBool("isHPCritical", false);
+                thing = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            anim.SetTrigger("takeDamage");
+        }
+    }
+}
