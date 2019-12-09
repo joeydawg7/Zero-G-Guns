b83df1e92e805075faaf0ae78a201e0d79036e32
Paul Boyko <paul@devilscider.com>
2019-12-09 18:35:38 -0500

    Gun Postion changes

--- NoGravityGuns/Assets/Scripts/GunPositionValue.cs
+++ NoGravityGuns/Assets/Scripts/GunPositionValue.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class GunPositionValue : MonoBehaviour
 {
     public Vector3 position;
-    [Header("Leave blank if none required")]
-    public string AnimationTrigger;
+    public Transform gunConnectionPoint;
+    public Vector3 handConnectionPoint;
+
+    //[Header("Leave blank if none required")]
+    //public string AnimationTrigger;
+
+    //public void SetHandPositions(ArmsScript arms)
+    //{
+    //    Animator animator = arms.GetComponent<Animator>();
+
+    //    if(animator !=null && AnimationTrigger!="")
+    //    {
+    //        animator.SetTrigger(AnimationTrigger);
+    //    }
+    //}
 
     public void SetHandPositions(ArmsScript arms)
     {
-        Animator animator = arms.GetComponent<Animator>();
-
-        if(animator !=null && AnimationTrigger!="")
-        {
-            animator.SetTrigger(AnimationTrigger);
-        }
-
+       
+        //this.gameObject.transform.localPosition = arms.connectionPoint.transform.localPosition - new Vector3(connectionPoint.position.x, 0.0f, 0.0f);
     }
 
 }
