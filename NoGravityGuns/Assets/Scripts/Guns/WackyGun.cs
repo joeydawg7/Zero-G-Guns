8aa8dcc4ccb5edce236a48336392e6b1d5f22a88
Ivy <Ivy@DESKTOP-3I7CF93>
2019-12-12 15:43:16 -0500

    Start work wackey Gun

---
+++ NoGravityGuns/Assets/Scripts/Guns/WackyGun.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WackyGun : Guns
+{
+    public string name;
+    public float knockBackModifier;
+    //public int minDamageRange;
+    //public int maxDamageRange;
+    //public float bulletSpeed;
+    
+    public float delayBeforeShot;
+    public List<Transform> bulletSpawns;
+
+    private void Start()
+    {
+       
+    }
+
+    public override void Fire(PlayerScript player)
+    {
+        if (CheckIfAbleToiFire(this))
+        {            
+            //player.armsScript.audioSource.PlayOneShot(GetRandomGunshotSFX);
+            player.StartCoroutine(ShootThisGun(player));
+            ReduceBullets(player);
+        }
+        else
+        {
+            CheckForAmmo(player);
+        }       
+    }
+
+    public IEnumerator ShootThisGun(PlayerScript player)
+    {
+        player.armsScript.bulletSpawn = bulletSpawns[Random.Range(0, bulletSpawns.Count)];
+        player.StartCoroutine(DelayShotCoroutine(player, delayBeforeShot, bulletSpeed, minDamageRange, maxDamageRange));
+        ReduceBullets(player);
+        yield return new WaitForSeconds(delayBeforeShot);
+        player.armsScript.bulletSpawn = bulletSpawns[Random.Range(0, bulletSpawns.Count)];
+        player.StartCoroutine(DelayShotCoroutine(player, delayBeforeShot, bulletSpeed, minDamageRange, maxDamageRange));
+        ReduceBullets(player);
+        yield return new WaitForSeconds(delayBeforeShot);
+        player.armsScript.bulletSpawn = bulletSpawns[Random.Range(0, bulletSpawns.Count)];
+        player.StartCoroutine(DelayShotCoroutine(player, delayBeforeShot, bulletSpeed, minDamageRange, maxDamageRange));
+        ReduceBullets(player);
+    }
+}
